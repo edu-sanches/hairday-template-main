992be3cb769bfb8abefc6408a8a0f1fad385cc9d
Eduardo Sanches <edupsanches@gmail.com>
2025-06-09 23:55:50 -0300

    feat: prepare infos to send to API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 // Date atual para o input
@@ -15,5 +16,40 @@ selectedDate.min = inputDate
 form.onsubmit = (event) => {
     event.preventDefault()
 
-    console.log("Enviado")
+    try {
+        // Recuperando o nome do cliente
+        const name = clientName.value.trim()
+        
+        // Exibe um alerta para informar um nome
+        if(!name) {
+            return alert("Informe o nome do cliente")
+        }
+
+        // Recupera o horário selecionado
+        const hourSelected = document.querySelector(".hour-selected")
+        
+        // Exibe um alerta caso nenhum horario selecionado
+        if(!hourSelected) {
+            return alert("Selecione um horário")
+        }
+        
+        // Recupera somente a hora
+        const [hour] = hourSelected.innerText.split(":")
+        
+        // Insere a hora na data
+        const when = dayjs(selectedDate.value).add(hour, "hour")
+
+        // Gera um ID
+        const id = new Date().getTime()
+
+        console.log({
+            id,
+            hour,
+            when
+        })
+
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento")
+        console.log(error)
+    }
 }
